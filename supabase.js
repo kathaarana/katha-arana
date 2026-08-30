ad094ecace92d194797801bb567af18cbf605b31
praveennavodya12345-design <praveennavodya12345@gmail.com>
2026-08-30 19:31:34 +0530

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,12 @@
+const SUPABASE_URL =
+    "https://klovgoywctcezqgyougv.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_RcvNjAOkExmL33MjjQNlaw_Qhl4xUu2";
+
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_PUBLISHABLE_KEY
+    );
