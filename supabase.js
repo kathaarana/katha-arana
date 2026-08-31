482447ee50062b390471c7c2fb289fa0ce09bd43
praveennavodya12345-design <praveennavodya12345@gmail.com>
2026-08-31 11:13:15 +0530

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,20 +1,11 @@
-```javascript
-// =========================================
-// KATHA ARANA
-// SUPABASE CONFIGURATION
-// =========================================
-
 const SUPABASE_URL =
     "https://klovgoywctcezqgyougv.supabase.co";
 
-
 const SUPABASE_PUBLISHABLE_KEY =
     "sb_publishable_RcvNjAOkExmL33MjjQNlaw_Qhl4xUu2";
-
 
 const supabaseClient =
     window.supabase.createClient(
         SUPABASE_URL,
         SUPABASE_PUBLISHABLE_KEY
     );
-```
